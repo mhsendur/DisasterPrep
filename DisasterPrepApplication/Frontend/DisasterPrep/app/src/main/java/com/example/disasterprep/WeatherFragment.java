463aedb689fe948aae0b825e89e1b97b5184d1e4
mhsendur <120842010+mhsendur@users.noreply.github.com>
2023-05-30 02:04:24 +0300

    changes

---
+++ DisasterPrepApplication/Frontend/DisasterPrep/app/src/main/java/com/example/disasterprep/WeatherFragment.java
@@ -0,0 +1,22 @@
+package com.example.disasterprep;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+
+    public WeatherFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        return rootView;
+    }
+}
